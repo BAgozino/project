2603afa917db6f3e922340ddf5632eeb57146f5c
Biko Agozino <ba325@cam.ac.uk>
2016-04-09 15:58:39 +0100

    distance metrics cyclic extension

--- src/main/java/uk/ac/cam/ba325/Matcher/DistanceMetric.java
+++ src/main/java/uk/ac/cam/ba325/Matcher/DistanceMetric.java
@@ -16,21 +16,21 @@ public class DistanceMetric {
      * @param sequence2
      * @return
      */
-    public static int twoDimensionalEditDistance(Sequence sequence1, Sequence sequence2,int offset){
+    public static int twoDimensionalEditDistance(Sequence sequence1, Sequence sequence2, int offset){
         int returnValue = 0;
-        returnValue += editDistance(sequence1.getBassDrums(),sequence2.getBassDrums());
-        returnValue += editDistance(sequence1.getCrashCymbals(),sequence2.getCrashCymbals());
-        returnValue += editDistance(sequence1.getFloorToms(),sequence2.getFloorToms());
-        returnValue += editDistance(sequence1.getHighHats(),sequence2.getHighHats());
-        returnValue += editDistance(sequence1.getHighToms(),sequence2.getHighToms());
-        returnValue += editDistance(sequence1.getLowToms(),sequence2.getLowToms());
-        returnValue += editDistance(sequence1.getRideCymbals(),sequence2.getRideCymbals());
-        returnValue += editDistance(sequence1.getSnareDrums(),sequence2.getSnareDrums());
+        returnValue += editDistance(sequence1.getBassDrums(),sequence2.getBassDrums(),offset);
+        returnValue += editDistance(sequence1.getCrashCymbals(),sequence2.getCrashCymbals(),offset);
+        returnValue += editDistance(sequence1.getFloorToms(),sequence2.getFloorToms(),offset);
+        returnValue += editDistance(sequence1.getHighHats(),sequence2.getHighHats(),offset);
+        returnValue += editDistance(sequence1.getHighToms(),sequence2.getHighToms(),offset);
+        returnValue += editDistance(sequence1.getLowToms(),sequence2.getLowToms(),offset);
+        returnValue += editDistance(sequence1.getRideCymbals(),sequence2.getRideCymbals(),offset);
+        returnValue += editDistance(sequence1.getSnareDrums(),sequence2.getSnareDrums(),offset);
 
         return returnValue;
     }
 
-    public static int editDistance(List<Strike> sequence1,List<Strike> sequence2){
+    public static int editDistance(List<Strike> sequence1,List<Strike> sequence2, int offset){
         int m = sequence1.size();
         int n = sequence2.size();
         int[][] distances = new int[m][n];
@@ -44,7 +44,7 @@ public class DistanceMetric {
 
         for(int j=1; j<n; j++){
             for(int i=1; i<m; i++){
-                if(sequence1.get(i).getValue() == sequence2.get(j).getValue()){
+                if(sequence1.get(i+offset %16).getValue() == sequence2.get(j+offset %16).getValue()){
                     distances[i][j] = distances[i-1][j-1];
                 }else{
                     distances[i][j] = min(distances[i-1][j] +1,distances[i][j-1] +1,distances[i-1][j-1] +1);
@@ -56,28 +56,54 @@ public class DistanceMetric {
         return distances[m][n];
     }
 
-    public static int twoDimensionalHammingDistance(Sequence sequence1, Sequence sequence2){
-        int returnValue =0;
-        returnValue += hammingDistance(sequence1.getBassDrums(),sequence2.getBassDrums());
-        returnValue += hammingDistance(sequence1.getCrashCymbals(),sequence2.getCrashCymbals());
-        returnValue += hammingDistance(sequence1.getFloorToms(),sequence2.getFloorToms());
-        returnValue += hammingDistance(sequence1.getHighHats(),sequence2.getHighHats());
-        returnValue += hammingDistance(sequence1.getHighToms(),sequence2.getHighToms());
-        returnValue += hammingDistance(sequence1.getLowToms(),sequence2.getLowToms());
-        returnValue += hammingDistance(sequence1.getRideCymbals(),sequence2.getRideCymbals());
-        returnValue += hammingDistance(sequence1.getSnareDrums(),sequence2.getSnareDrums());
+    public static int twoDimensionalCyclicEditDistance(Sequence sequence1, Sequence sequence2){
+        int bestMatch = Integer.MAX_VALUE;
+        int currentDistance =0;
+        for(int i=0; i<sequence1.getResolution(); i++){
+            currentDistance = twoDimensionalEditDistance(sequence1,sequence2,i);
+            if(currentDistance<bestMatch){
+                bestMatch = currentDistance;
+            }
+        }
+        return bestMatch;
+    }
 
+    public static int twoDimensionalCyclicHammingDistance(Sequence sequence1, Sequence sequence2){
+        int bestMatch =Integer.MAX_VALUE;
+        int currentDistance = 0;
+        for(int i=0; i<sequence1.getResolution(); i++){
+            currentDistance = twoDimensionalHammingDistance(sequence1,sequence2,i);
+            if(currentDistance< bestMatch){
+                bestMatch = currentDistance;
+            }
+        }
+        return bestMatch;
+    }
+
+    public static int twoDimensionalHammingDistance(Sequence sequence1, Sequence sequence2,int offset){
+        int returnValue =0;
+        returnValue += hammingDistance(sequence1.getBassDrums(),sequence2.getBassDrums(),offset);
+        returnValue += hammingDistance(sequence1.getCrashCymbals(),sequence2.getCrashCymbals(),offset);
+        returnValue += hammingDistance(sequence1.getFloorToms(),sequence2.getFloorToms(),offset);
+        returnValue += hammingDistance(sequence1.getHighHats(),sequence2.getHighHats(),offset);
+        returnValue += hammingDistance(sequence1.getHighToms(),sequence2.getHighToms(),offset);
+        returnValue += hammingDistance(sequence1.getLowToms(),sequence2.getLowToms(),offset);
+        returnValue += hammingDistance(sequence1.getRideCymbals(),sequence2.getRideCymbals(),offset);
+        returnValue += hammingDistance(sequence1.getSnareDrums(),sequence2.getSnareDrums(),offset);
         return returnValue;
     }
 
-    public static int hammingDistance(List<Strike> sequence1, List<Strike> sequence2 ){
+    public static int hammingDistance(List<Strike> sequence1, List<Strike> sequence2,int offset){
         int shorter = Math.min(sequence1.size(),sequence2.size());
         int longer = Math.max(sequence1.size(),sequence2.size());
 
         int returnValue = 0;
 
-        for(int i=0; i<shorter; i++){
-            if(sequence1.get(i) != sequence2.get(i)){
+
+        int pointInList;
+        for(int i=offset; i<shorter; i++){
+            pointInList = i % 16;
+            if(sequence1.get(pointInList) != sequence2.get(pointInList)){
                 returnValue++;
             }
         }
